565d8c62b90843f57257b9c0a23bc95374e73ad7
karthik <karthikg940@gmail.com>
2021-06-22 14:52:44 +0530

    Demo Application Commit 1.

--- src/main/java/com/imimobile/Demo.java
+++ src/main/java/com/imimobile/Demo.java
@@ -3,5 +3,6 @@ package com.imimobile;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("this is the demo");
+        System.out.println("this is the demo 2");
     }
 }
